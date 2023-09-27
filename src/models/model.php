c51deaabb6368c13e07bb61b855f97ead7f9c31b
vicmtn <victorien.montavon@gmail.com>
2023-09-27 19:17:58 +0200

    rework abstract model.php, need to check if the PDO class is really working

--- src/models/model.php
+++ src/models/model.php
@@ -2,30 +2,31 @@
 
 /**
  * Title : Abstract Class Model
+ * Description : Abstract model for all models
  * Author : Montavon Victorien
- * Verison : 1.0.0
- * Date : 30.08.2023
+ * Creation date : 13.09.2023
+ * Version : 1.0
  */
+
 
 abstract class Model
 {
-    private $host;
-    private $db;
-    private $user;
-    private $password;
+    protected $host;
+    protected $db;
+    protected $dbuser;
+    protected $dbpassword;
 
-    public function __construct($host, $db, $user, $password)
-    {
-        $this->host = $host;
-        $this->db = $db;
-        $this->user = $user;
-        $this->password = $password;
-    }
 
-    private function connect()
+    public function connect()
     {
+        $this->host = $_ENV['DB_HOST'];
+        $this->dbname = $_ENV['DB_DATABASE'];
+        $this->dbuser = $_ENV['DB_USERNAME'];
+        $this->dbpassword = $_ENV['DB_PASSWORD'];
+
         try {
-            $pdo = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->password);
+            $dsn = "mysql:host=$this->host;dbname=$this->dbname";
+            $pdo = new PDO($dsn, $this->dbuser, $this->dbpassword);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
         } catch (PDOException $e) {
