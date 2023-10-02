2bf83281e6e9f503dcf41ec606416ec55fe3aa51
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-10-02 22:08:11 +0200

    Refactor routing by path

--- src/routing/router.php
+++ src/routing/router.php
@@ -11,37 +11,50 @@ class Router
     {
         $this->setRoutes();
     }
-    public function setRoutes()
+    private function setRoutes()
     {
         $routes = include('routeConfig.php');
+
         foreach ($routes as $route => $param) {
-            $this->routes[] = new Route($param['path'], $param['controller'], $param['method']);
+            $this->routes[] = new Route($route, $param['path'], $param['controller'], $param['method']);
         }
     }
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
     //function to redirect to the right controller
-    public function redirect($path)
+    public function redirectByPath($path)
+    {
+        $this->redirect($this->getRouteByPath($path));
+    }
+    public function getRouteByPath($path)
     {
         foreach ($this->routes as $route) {
             if ($route->getPath() == $path) {
-                $controllerName = $route->getController();
-                $method = $route->getMethod();
-
-                // Check if the controller class exists
-                if (class_exists($controllerName)) {
-                    $controller = new $controllerName();
-
-                    // Check if the method exists within the controller class
-                    if (method_exists($controller, $method)) {
-                        $controller->$method();
-                    } else {
-                        // Handle method not found error
-                        echo "Method not found in controller: $method";
-                    }
-                } else {
-                    // Handle controller not found error
-                    echo "Controller not found: $controllerName";
-                }
+                return $route;
             }
+        }
+    }
+    public function redirect(Route $route)
+    {
+        $controllerName = $route->getController();
+        $method = $route->getMethod();
+
+        // Check if the controller class exists
+        if (class_exists($controllerName)) {
+            $controller = new $controllerName();
+
+            // Check if the method exists within the controller class
+            if (method_exists($controller, $method)) {
+                $controller->$method();
+            } else {
+                // Handle method not found error
+                echo "Method not found in controller: $method";
+            }
+        } else {
+            // Handle controller not found error
+            echo "Controller not found: $controllerName";
         }
     }
 }
