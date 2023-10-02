926025022ffe657d883df661949bbf3ba2d78bb2
vicmtn <victorien.montavon@gmail.com>
2023-10-02 22:50:51 +0200

    Create enum Status for Exercises

---
+++ src/enums/Status.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title : Enum Status
+ * Author : Montavon Victorien
+ * Version : 1.0.0
+ * Date : 14.09.2023
+ */
+
+namespace App\enums;
+enum Status
+{
+
+    case Building;
+    case Answering;
+    case Closed;
+}
