14574ae3b3b3067f942944edea94c8f1e886ef0d
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-10-04 09:39:20 +0200

    Create the router in the public index

--- public/index.php
+++ public/index.php
@@ -10,6 +10,12 @@
 // Require the autoload file
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
+use App\Routing\Router;
+
 // Load the environment variables
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+// Create the router
+$router = new Router();
+$router->redirectByPath($_SERVER['REQUEST_URI']);
