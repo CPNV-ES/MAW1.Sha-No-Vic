b93c141b91a6a04eed5f842a403d80ebaccf9e5b
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-10-04 13:39:36 +0200

    Fullify the method match in route class

--- src/routing/route.php
+++ src/routing/route.php
@@ -37,7 +37,29 @@ class Route
     {
         return $this->httpMethod;
     }
-    public function match()
+    public function match($path)
     {
+        if ($this->path == $path && $this->httpMethod == $_SERVER['REQUEST_METHOD']) {
+            //check the path to see if there is a parameter
+            if (strpos($path, ':') !== false) {
+                //if there is a parameter, we need to check if the parameter is an id
+                $pathArray = explode('/', $path);
+                $routeArray = explode('/', $this->path);
+                //check if the parameter is an id
+                foreach ($routeArray as $key => $value) {
+                    if (strpos($value, ':') !== false) {
+                        //if the parameter is an id, we need to check if the id is an integer
+                        if (is_numeric($pathArray[$key])) {
+                            //if the id is an integer, we can return true
+                            return true;
+                        }
+                    }
+                }
+                //if the parameter is not an id, we can return true
+                return true;
+            }
+            return true;
+        }
+        return false;
     }
 }
