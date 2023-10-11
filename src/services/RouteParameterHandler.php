e3323be70eaf77376483a926a87d6dc8a867b4a4
ShansheGundishvili <pw07vck@eduvaud.ch>
2023-10-11 10:43:15 +0200

    added new function insertIdInURL in class RouteParameterHandler

--- src/services/RouteParameterHandler.php
+++ src/services/RouteParameterHandler.php
@@ -2,4 +2,16 @@
 class RouteParameterHandler
 {
 
+    public function insertIdInURL($url, $ids){
+        $url = explode('/', $url);
+        $i = 0;
+        foreach ($url as $key => $value) {
+            if (strpos($value, ':') !== false) {
+                $url[$key] = $ids[$i];
+                $i++;
+            }
+        }
+        return implode('/', $url);
+    }
+
 }
