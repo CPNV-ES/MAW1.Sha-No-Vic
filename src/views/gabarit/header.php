bc170e71b4b37fe1468e8777f9329907f9251aed
Shanshe <Shanshe.gundishvili@eduvaud.ch>
2023-11-08 10:25:34 +0100

    added default footer, head and header in the gabarits folder

---
+++ src/views/gabarit/header.php
@@ -0,0 +1,7 @@
+    <body>
+        <header class="dashboard">
+            <section class="container">
+                <p><img src="../css/logo.png"></p>
+                <h1><?= $data['title'] ?></h1>
+            </section>
+        </header>
