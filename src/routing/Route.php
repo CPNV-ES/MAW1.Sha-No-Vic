65d1daf22d6586da4c912a2f2ee593d869b1cb32
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-11-08 10:04:29 +0100

    Refactor match function of route

--- src/routing/Route.php
+++ src/routing/Route.php
@@ -39,27 +39,22 @@ class Route
     }
     public function match($path)
     {
+        //TODO: ajouter un match sur plusieur methode http par route en mettant la posibilité de mettre une methdoe en post et get par exemple
+        $path = $this->routeTemplateFromURL($path);
+
         if ($this->path == $path && $this->httpMethod == $_SERVER['REQUEST_METHOD']) {
-            //check the path to see if there is a parameter
-            if (strpos($path, ':') !== false) {
-                //if there is a parameter, we need to check if the parameter is an id
-                $pathArray = explode('/', $path);
-                $routeArray = explode('/', $this->path);
-                //check if the parameter is an id
-                foreach ($routeArray as $key => $value) {
-                    if (strpos($value, ':') !== false) {
-                        //if the parameter is an id, we need to check if the id is an integer
-                        if (is_numeric($pathArray[$key])) {
-                            //if the id is an integer, we can return true
-                            return true;
-                        }
-                    }
-                }
-                //if the parameter is not an id, we can return true
-                return true;
-            }
             return true;
         }
         return false;
+    }
+    public function routeTemplateFromURL($url)
+    {
+        $url = explode('/', $url);
+        foreach ($url as $key => $value) {
+            if (is_numeric($value)) {
+                $url[$key] = ':id';
+            }
+        }
+        return implode('/', $url);
     }
 }
