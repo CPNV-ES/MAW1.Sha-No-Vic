04896ee276b98c6df46550a7740528c37a310083
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-11-10 09:19:25 +0100

    Fix head path

--- src/views/gabarits/head.php
+++ src/views/gabarits/head.php
@@ -7,21 +7,21 @@
         <title>ExerciseLooper</title>
         <meta name="csrf-param" content="authenticity_token">
         <meta name="csrf-token" content="bdMGbw2uBQI7s9/RVaEJfV3ipzEME3hfuIJn+hH/fulEZotbYUX3guzRNq9rXi1w+zE7on4X3c0vL2tIDQyZaw==">
-        <link rel="stylesheet" media="all" href="../../../public/css/normalize.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/milligram.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/exercises.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/fields.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/fontawesome-solid.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/fontawesome.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/fulfillments.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/heading.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/results.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/scaffold.css">
-        <link rel="stylesheet" media="all" href="../../../public/css/application.css">
-        <script src="../../../public/js/rails-ujs.js"></script>
-        <script src="../../../public/js/exercises.js"></script>
-        <script src="../../../public/js/fields.js"></script>
-        <script src="../../../public/js/fulfillments.js"></script>
-        <script src="../../../public/js/results.js"></script>
-        <script src="../../../public/js/application.js"></script>
+        <link rel="stylesheet" media="all" href="/css/normalize.css">
+        <link rel="stylesheet" media="all" href="/css/milligram.css">
+        <link rel="stylesheet" media="all" href="/css/exercises.css">
+        <link rel="stylesheet" media="all" href="/css/fields.css">
+        <link rel="stylesheet" media="all" href="/css/fontawesome-solid.css">
+        <link rel="stylesheet" media="all" href="/css/fontawesome.css">
+        <link rel="stylesheet" media="all" href="/css/fulfillments.css">
+        <link rel="stylesheet" media="all" href="/css/heading.css">
+        <link rel="stylesheet" media="all" href="/css/results.css">
+        <link rel="stylesheet" media="all" href="/css/scaffold.css">
+        <link rel="stylesheet" media="all" href="/css/application.css">
+        <script src="/js/rails-ujs.js"></script>
+        <script src="/js/exercises.js"></script>
+        <script src="/js/fields.js"></script>
+        <script src="/js/fulfillments.js"></script>
+        <script src="/js/results.js"></script>
+        <script src="/js/application.js"></script>
 </head>
