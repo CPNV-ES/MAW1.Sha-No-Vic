9821580f5e6fb39f91e4e0be58a65cbf0abaf5fe
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-11-10 09:11:32 +0100

    Remove old gabarit

--- src/views/gabarit/header.php
+++
@@ -1,7 +0,0 @@
-    <body>
-        <header class="dashboard">
-            <section class="container">
-                <p><img src="../css/logo.png"></p>
-                <h1><?= $data['title'] ?></h1>
-            </section>
-        </header>
