3f767201637590ed5f59c3c04a05053331292c52
Shanshe <Shanshe.gundishvili@eduvaud.ch>
2023-11-10 09:05:28 +0100

    fix : css nd js path fixes

---
+++ src/views/gabarits/head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!-- saved from url=(0033)http://exercice-looper.mycpnv.ch/ -->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>ExerciseLooper</title>
+        <meta name="csrf-param" content="authenticity_token">
+        <meta name="csrf-token" content="bdMGbw2uBQI7s9/RVaEJfV3ipzEME3hfuIJn+hH/fulEZotbYUX3guzRNq9rXi1w+zE7on4X3c0vL2tIDQyZaw==">
+        <link rel="stylesheet" media="all" href="../../../public/css/normalize.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/milligram.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/exercises.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/fields.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/fontawesome-solid.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/fontawesome.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/fulfillments.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/heading.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/results.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/scaffold.css">
+        <link rel="stylesheet" media="all" href="../../../public/css/application.css">
+        <script src="../../../public/js/rails-ujs.js"></script>
+        <script src="../../../public/js/exercises.js"></script>
+        <script src="../../../public/js/fields.js"></script>
+        <script src="../../../public/js/fulfillments.js"></script>
+        <script src="../../../public/js/results.js"></script>
+        <script src="../../../public/js/application.js"></script>
+    </head>
