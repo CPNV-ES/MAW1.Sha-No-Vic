74bbff9e924b6fc312c83f99edb84f63d8d4a02f
vicmtn <victorien.montavon@gmail.com>
2023-11-15 10:31:14 +0100

    rework pdo and remove getAll() method

--- src/models/Model.php
+++ src/models/Model.php
@@ -15,55 +15,28 @@ use PDO;
 
 abstract class Model
 {
-    public static $pdo;
+    public static PDO $pdo;
     protected static $table;
 
     protected $db;
     protected $dbuser;
     protected $dbpassword;
 
-    /**
-     * Constructor of the model
-     * @param $host , @param $db, @param $dbuser, @param $dbpassword
-     * @return void, create a new model
-     */
 
-    public function __construct()
+    public static function getConnection(): PDO
     {
-        self::setupConnection();
-    }
-
-    public static function setupConnection()
-    {
+        if (isset(self::$pdo)) return self::$pdo;
         $host = $_ENV['DB_HOST'];
         $dbname = $_ENV['DB_DATABASE'];
         $dbuser = $_ENV['DB_USERNAME'];
         $dbpassword = $_ENV['DB_PASSWORD'];
 
-        if (self::$pdo == null) {
-            $dsn = "mysql:host=$host;dbname=$dbname";
-            $pdo = new PDO($dsn, $dbuser, $dbpassword);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$pdo = $pdo;
-            return $pdo;
-        }
+        $dsn = "mysql:host=$host;dbname=$dbname";
+        $pdo = new PDO($dsn, $dbuser, $dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        self::$pdo = $pdo;
+        return $pdo;
 
     }
 
-    /**
-     * Method to get all exercises from database
-     * @return array of exercises
-     * @throws Exception
-     */
-    public static function getAll()
-    {
-        if (self::$table == null) {
-            throw new Exception("Table does not exist");
-        } else {
-            $query = "SELECT * FROM " . self::$table;
-            $stmt = self::$pdo->prepare($query);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_CLASS, self::class);
-        }
-    }
 }
