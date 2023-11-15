b4c84877e7fee63cbe1940025e5bfd0656f976cd
vicmtn <victorien.montavon@gmail.com>
2023-11-15 10:38:34 +0100

    refactor name

---
+++ src/controllers/ExercisesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\controllers;
+
+use App\models\Exercise;
+use App\models\Model;
+use App\Services\Renderer;
+
+class ExercisesController
+{
+    public function takeExercises(): void
+    {
+        $data['title'] = 'Take an exercise';
+        $status = "'answering'";
+        $data['exercises'] = Exercise::getAll($status);
+        $renderer = new Renderer();
+        $view = '../views/exercises/answering.view.php';
+        $renderer->renderView($renderer->createView($view, $data));
+    }
+
+}
