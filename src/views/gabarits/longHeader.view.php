38d96f60255f7d471252428ac5c5b769af9d687c
Shanshe <Shanshe.gundishvili@eduvaud.ch>
2023-11-17 08:24:19 +0100

    fix img.png path

--- src/views/gabarits/longHeader.view.php
+++ src/views/gabarits/longHeader.view.php
@@ -1,7 +1,7 @@
 <body>
     <header class="dashboard">
         <section class="container">
-            <p><img src="assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png" /></p>
+            <p><img src="/img/logo.png" /></p>
             <?= $data['title'] ?>
         </section>
     </header>
