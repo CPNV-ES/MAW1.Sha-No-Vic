77a8d357c8012f32176c580a5f4d89bc18da4b7e
vicmtn <victorien.montavon@gmail.com>
2023-11-22 08:22:56 +0100

    add link to redirect to the right file

--- src/views/exercises/answering.php
+++ src/views/exercises/answering.php
@@ -4,7 +4,7 @@
         <li class="row">
             <div class="column card">
                 <div class="title"><?= $exercise->getTitle(); ?></div>
-                <a class="button" href="#">Take it</a>
+                <a class="button" href="exercises/<?= $exercise->getId()?>/fulfillments/new">Take it</a>
             </div>
         </li>
     <?php
