3634dc85d52ec27b655d3776df0bf45584b3530e
vicmtn <victorien.montavon@gmail.com>
2023-11-29 08:26:54 +0100

    rework fake insertion script

--- conception/dataBase/fake.exercises.sql
+++ conception/dataBase/fake.exercises.sql
@@ -1,41 +1,41 @@
 use exercise_looper;
-insert into exercises (id, title, status, creation_date, modification_date)
-values (1, 'The Skinny', 'answering', '2012-09-17', '2013-02-11');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (2, 'Seven Beauties (Pasqualino Settebellezze)', 'closed', '2002-11-01', '2001-11-12');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (3, 'Savannah', 'answering', '2022-11-15', '2017-03-06');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (4, 'Love and Other Disasters', 'building', '2011-09-08', '2012-04-22');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (5, 'Faces', 'closed', '2005-10-03', '2022-09-03');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (6, 'Inbetween Worlds', 'closed', '2005-09-05', '2020-07-25');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (7, 'Night of the Comet', 'closed', '2022-07-31', '2019-01-17');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (8, '100 Rifles', 'building', '2002-02-08', '2009-01-10');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (9, 'How to Rob a Bank', 'answering', '2021-09-25', '2014-01-28');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (10, 'Triumph of the Nerds, The: The Rise of Accidental Empires', 'closed', '2020-02-28', '2000-08-25');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (11, 'Dog''s Breakfast, A', 'closed', '2002-01-12', '2005-04-27');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (12, 'Leopard Son, The', 'building', '2007-10-05', '2019-08-23');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (13, 'Lady and the Tramp', 'building', '2009-03-16', '2011-03-17');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (14, 'Power and Terror: Noam Chomsky in Our Times', 'building', '2010-11-02', '2021-03-09');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (15, 'I''m Going Home (Je rentre à la maison)', 'closed', '2019-12-02', '2011-02-22');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (16, 'Leather Jacket Love Story', 'building', '2014-07-10', '2010-12-30');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (17, 'City Lights', 'building', '2008-03-23', '2012-02-18');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (18, 'Vacuuming Completely Nude in Paradise', 'closed', '2008-11-30', '2017-08-06');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (19, 'The Count of Monte Cristo', 'building', '2018-11-25', '2006-09-30');
-insert into exercises (id, title, status, creation_date, modification_date)
-values (20, 'To Be or Not to Be', 'closed', '2011-03-25', '2021-04-07');
+insert into exercises (id, title, status)
+values (1, 'The Skinny', 'answering');
+insert into exercises (id, title, status)
+values (2, 'Seven Beauties (Pasqualino Settebellezze)', 'closed');
+insert into exercises (id, title, status)
+values (3, 'Savannah', 'answering');
+insert into exercises (id, title, status)
+values (4, 'Love and Other Disasters', 'building');
+insert into exercises (id, title, status)
+values (5, 'Faces', 'closed');
+insert into exercises (id, title, status)
+values (6, 'Inbetween Worlds', 'closed');
+insert into exercises (id, title, status)
+values (7, 'Night of the Comet', 'closed');
+insert into exercises (id, title, status)
+values (8, '100 Rifles', 'building');
+insert into exercises (id, title, status)
+values (9, 'How to Rob a Bank', 'answering');
+insert into exercises (id, title, status)
+values (10, 'Triumph of the Nerds, The: The Rise of Accidental Empires', 'closed');
+insert into exercises (id, title, status)
+values (11, 'Dog''s Breakfast, A', 'closed');
+insert into exercises (id, title, status)
+values (12, 'Leopard Son, The', 'building');
+insert into exercises (id, title, status)
+values (13, 'Lady and the Tramp', 'building');
+insert into exercises (id, title, status)
+values (14, 'Power and Terror: Noam Chomsky in Our Times', 'building');
+insert into exercises (id, title, status)
+values (15, 'I''m Going Home (Je rentre à la maison)', 'closed');
+insert into exercises (id, title, status)
+values (16, 'Leather Jacket Love Story', 'building');
+insert into exercises (id, title, status)
+values (17, 'City Lights', 'building');
+insert into exercises (id, title, status)
+values (18, 'Vacuuming Completely Nude in Paradise', 'closed');
+insert into exercises (id, title, status)
+values (19, 'The Count of Monte Cristo', 'building');
+insert into exercises (id, title, status)
+values (20, 'To Be or Not to Be', 'closed');
