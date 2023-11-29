b040bea4c9ccf36a9ac0d1cb5cecb7baf59128cf
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-11-29 08:27:36 +0100

    Add Methode newExercise in ExercisesController

--- src/controllers/ExercisesController.php
+++ src/controllers/ExercisesController.php
@@ -18,11 +18,20 @@ class ExercisesController
         $renderer->renderView($renderer->createView($view, $data));
     }
 
-    public function takeOneExercise($exercise_id): void {
+    public function takeOneExercise($exercise_id): void
+    {
         $data['title'] = Exercise::getTitleById($exercise_id);
         $data['questions'] = Question::GetQuestionByExercise($exercise_id);
         $renderer = new Renderer();
         $view = '../views/fulfillments/fulfillmentsNew.php';
+        $renderer->renderView($renderer->createView($view, $data));
+    }
+    public function newExercise(): void
+    {
+        $data['title'] = 'New exercise';
+        $renderer = new Renderer();
+        $view = '../views/exercises/new.php';
+        dd($data);
         $renderer->renderView($renderer->createView($view, $data));
     }
 }
