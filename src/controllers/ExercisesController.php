05c047c3c4de9a77bcab8ff996738994ae51019b
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-12-01 08:20:13 +0100

    Add manaageField Function in exercises controller

--- src/controllers/ExercisesController.php
+++ src/controllers/ExercisesController.php
@@ -38,4 +38,12 @@ class ExercisesController
         $exercise = Exercise::save($_POST['title'], 'building');
         header('Location: /exercises/' . $exercise . '/fields');
     }
+    public function manageFields($params): void
+    {
+        $data['title'] = 'Manage fields';
+        $data['exercise_id'] = $params[0];
+        $renderer = new Renderer();
+        $view = '../views/exercises/Fields.php';
+        $renderer->renderView($renderer->createView($view, $data));
+    }
 }
