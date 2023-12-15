a9d0df86edc5a7c71544adddc2196a0bad80ef0c
vicmtn <victorien.montavon@gmail.com>
2023-12-15 11:12:33 +0100

    remove unused space

--- src/views/fulfillments/fulfillmentsNew.php
+++ src/views/fulfillments/fulfillmentsNew.php
@@ -2,13 +2,10 @@
 <main class="container">
     <h1>Your take</h1>
     <p>If you'd like to come back later to finish, simply submit it with blanks</p>
-
     <form action="/exercises/<?= $data['exercise_id']; ?>/fulfillments" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="7d+vB3MFNW/SkvfwldHqj4osZMOnHQmKod4m/iXa4q0V1mFpAat6j3S46ZTxwVYrvPvcdHHbFLYOc1JXVJZT5w==">
 
         <?php foreach ($data['questions'] as $question) : ?>
-
         <?php if ($question->getType() == "single_line") : ?>
-        <input type="hidden" value="<?= $question->getId(); ?>" name="fulfillment[answers_attributes][][field_id]" id="fulfillment_answers_attributes__field_id">
         <div class="field">
             <label for="fulfillment_answers_attributes__value"><?= $question->getTitle(); ?></label>
             <input type="text" name="fulfillment[answers_attributes][][value]" id="fulfillment_answers_attributes__value">
