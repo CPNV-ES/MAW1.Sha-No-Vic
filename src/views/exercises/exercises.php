35027d30ce5d0316ebe41fa6842591236f19bc85
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-12-20 10:16:30 +0100

    Update Exercese.php to prevent false changing status

--- src/views/exercises/exercises.php
+++ src/views/exercises/exercises.php
@@ -3,34 +3,40 @@
         <h1>Building</h1>
         <table class="records">
             <thead>
-            <tr>
-                <th>Title</th>
-                <th></th>
-            </tr>
+                <tr>
+                    <th>Title</th>
+                    <th></th>
+                </tr>
             </thead>
             <tbody>
-            <?php foreach ($data['exercises'] as  $exercise) : if ($exercise->getStatus() == "building") : ?>
-            <tr>
-                <td class="no-css-td" class="text-widen"><?= $exercise->getTitle() ?></td>
-                <td class="no-css-td">
-                    <div class="container widen">
+                <?php foreach ($data['exercises'] as $exercise):
+                    if ($exercise->getStatus() == "building"): ?>
+                        <tr>
+                            <td class="no-css-td" class="text-widen">
+                                <?= $exercise->getTitle() ?>
+                            </td>
+                            <td class="no-css-td">
+                                <div class="container widen">
 
-                        <?php if($exercise->hasQuestions()) : ?>
-                        <form id="changeExerciseStatus" method="post" class="icon-form action-icon" action="exercises/<?= $exercise->getId() ?>">
-                            <input type="hidden" name="_method" value="PUT">
-                            <button class="no-css" type="submit"><i class="fa fa-comment purple"></i></button>
-                        </form>
-                        <?php endif; ?>
+                                    <?php if ($exercise->hasQuestions()): ?>
+                                        <form id="changeExerciseStatus" method="post" class="icon-form action-icon"
+                                            action="exercises/<?= $exercise->getId() ?>">
+                                            <input type="hidden" name="_method" value="PUT">
+                                            <button class="no-css" type="submit"><i class="fa fa-comment purple"></i></button>
+                                        </form>
+                                    <?php endif; ?>
 
-                        <a title="Manage fields" href="exercises/<?= $exercise->getId() ?>/fields"><i class="fa fa-edit action-icon"></i></a>
-                        <form id="deleteForm" method="post" class="icon-form action-icon" action="exercises/<?= $exercise->getId() ?>">
-                            <input type="hidden" name="_method" value="DELETE">
-                            <button class="no-css" type="submit"><i class="fa fa-trash purple"></i></button>
-                        </form>
-                    </div>
-                </td>
-            </tr>
-            <?php endif; endforeach; ?>
+                                    <a title="Manage fields" href="exercises/<?= $exercise->getId() ?>/fields"><i
+                                            class="fa fa-edit action-icon"></i></a>
+                                    <form id="deleteForm" method="post" class="icon-form action-icon"
+                                        action="exercises/<?= $exercise->getId() ?>">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <button class="no-css" type="submit"><i class="fa fa-trash purple"></i></button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endif; endforeach; ?>
             </tbody>
         </table>
     </section>
@@ -39,24 +45,29 @@
         <h1>Answering</h1>
         <table class="records">
             <thead>
-            <tr>
-                <th>Title</th>
-                <th></th>
-            </tr>
+                <tr>
+                    <th>Title</th>
+                    <th></th>
+                </tr>
             </thead>
             <tbody>
-            <?php foreach ($data['exercises'] as  $exercise) : if ($exercise->getStatus() == "answering") : ?>
-            <tr>
-                <td><?= $exercise->getTitle() ?></td>
-                <td class="no-css-td">
-                    <a title="Show results" href="exercises/<?= $exercise->getId() ?>/results"><i class="fa fa-chart-bar"></i></a>
-                    <form id="changeExerciseStatus" method="post" class="icon-form action-icon" action="exercises/<?= $exercise->getId() ?>">
-                        <input type="hidden" name="_method" value="PUT">
-                        <button class="no-css" type="submit"><i class="fa fa-minus-circle purple"></i></button>
-                    </form>
-                </td>
-            </tr>
-            <?php endif; endforeach; ?>
+                <?php foreach ($data['exercises'] as $exercise):
+                    if ($exercise->getStatus() == "answering"): ?>
+                        <tr>
+                            <td>
+                                <?= $exercise->getTitle() ?>
+                            </td>
+                            <td class="no-css-td">
+                                <a title="Show results" href="exercises/<?= $exercise->getId() ?>/results"><i
+                                        class="fa fa-chart-bar"></i></a>
+                                <form id="changeExerciseStatus" method="post" class="icon-form action-icon"
+                                    action="exercises/<?= $exercise->getId() ?>">
+                                    <input type="hidden" name="_method" value="PUT">
+                                    <button class="no-css" type="submit"><i class="fa fa-minus-circle purple"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endif; endforeach; ?>
             </tbody>
         </table>
     </section>
@@ -65,25 +76,30 @@
         <h1>Closed</h1>
         <table class="records">
             <thead>
-            <tr>
-                <th>Title</th>
-                <th></th>
-            </tr>
+                <tr>
+                    <th>Title</th>
+                    <th></th>
+                </tr>
             </thead>
 
             <tbody>
-            <?php foreach ($data['exercises'] as  $exercise) : if ($exercise->getStatus() == "closed") : ?>
-            <tr>
-                <td><?= $exercise->getTitle() ?></td>
-                <td class="no-css-td">
-                    <a title="Show results" href="exercises/<?= $exercise->getId() ?>/results"><i class="fa fa-chart-bar"></i></a>
-                    <form id="deleteForm" method="post" class="icon-form action-icon" action="exercises/<?= $exercise->getId() ?>">
-                        <input type="hidden" name="_method" value="DELETE">
-                        <button class="no-css" type="submit"><i class="fa fa-trash purple"></i></button>
-                    </form>
-                </td>
-            </tr>
-            <?php endif; endforeach; ?>
+                <?php foreach ($data['exercises'] as $exercise):
+                    if ($exercise->getStatus() == "closed"): ?>
+                        <tr>
+                            <td>
+                                <?= $exercise->getTitle() ?>
+                            </td>
+                            <td class="no-css-td">
+                                <a title="Show results" href="exercises/<?= $exercise->getId() ?>/results"><i
+                                        class="fa fa-chart-bar"></i></a>
+                                <form id="deleteForm" method="post" class="icon-form action-icon"
+                                    action="exercises/<?= $exercise->getId() ?>">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <button class="no-css" type="submit"><i class="fa fa-trash purple"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endif; endforeach; ?>
             </tbody>
 
         </table>
