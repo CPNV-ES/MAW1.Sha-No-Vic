d250c0d8ae7426377a440cca1b07552d539fe770
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-12-20 10:19:34 +0100

    Update the call to header in index

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -9,7 +9,7 @@ class IndexController
     public function index(): void
     {
         $data['title'] = 'Exercise Looper';
-        $render = new Renderer(header: __DIR__ . '/../views/gabarits/longHeader.php');
+        $render = new Renderer(header: 'gabarits/longHeader.php');
         $view = $render->createView('../views/site/index.php', $data);
         $render->renderView($view);
     }
