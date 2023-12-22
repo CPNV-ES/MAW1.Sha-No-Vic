3afc10433d00f6d9f18c044881ee88f0b2970c94
NoahDelgado <noah.delgado1@eduvaud.ch>
2023-12-22 09:58:28 +0100

    Add CSRF token input into form of new exercises

--- src/views/exercises/new.php
+++ src/views/exercises/new.php
@@ -1,7 +1,7 @@
 <h1>New Exercise</h1>
 
 <form action="/exercises" accept-charset="UTF-8" method="post">
-
+  <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
   <div class="field">
     <label for="exercise_title">Title</label>
     <input type="text" name="title" id="exercise_title" />
