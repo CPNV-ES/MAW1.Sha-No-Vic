42c15cb41c51ed90b77856904cd1818b0f7736c6
Shanshe Gundishvili <ShansheGundishvili@eduvaud.ch>
2023-12-24 16:29:47 +0100

    fix to accomodate mac with null access (Potential)

--- src/views/gabarits/shortHeader.php
+++ src/views/gabarits/shortHeader.php
@@ -1,10 +1,10 @@
 <body>
-    <header class="heading <?= htmlspecialchars($data['color'] , ENT_QUOTES, 'UTF-8') ?? NULL; ?>">
+    <header class="heading <?= isset($data['header']['color']) ? htmlspecialchars($data['color'] , ENT_QUOTES, 'UTF-8') : NULL ?>">
         <section class="container">
             <a href="/"><img src="/img/logo.png" /></a>
-            <p class="exercise-label"><?= $data['header']['type'] ? htmlspecialchars($data['header']['type'] , ENT_QUOTES, 'UTF-8') . " :" : NULL; ?><b>
-                <a href="<?= htmlspecialchars($data['header']['link'] , ENT_QUOTES, 'UTF-8') ?? NULL ?>">
-                    <?= htmlspecialchars($data['header']['title'] , ENT_QUOTES, 'UTF-8') ?? NULL ?></b></a><?= isset($data['header']['staticTitle']) ? htmlspecialchars($data['header']['staticTitle'], ENT_QUOTES, 'UTF-8') : NULL ?></p>
+            <p class="exercise-label"><?= isset($data['header']['type']) ? htmlspecialchars($data['header']['type'] , ENT_QUOTES, 'UTF-8') . " :" : NULL ?><b>
+                <a href="<?= isset($data['header']['link']) ? htmlspecialchars($data['header']['link'] , ENT_QUOTES, 'UTF-8') : NULL ?>">
+                    <?= isset($data['header']['title']) ? htmlspecialchars($data['header']['title'] , ENT_QUOTES, 'UTF-8') : NULL ?></b></a><?= isset($data['header']['staticTitle']) ? htmlspecialchars($data['header']['staticTitle'], ENT_QUOTES, 'UTF-8') : NULL ?></p>
         </section>
     </header>
 <main class="container">
